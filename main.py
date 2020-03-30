9f987a2e786c3243240872890c54f4b52c3297f3
Haris Rashid <harisrashid.2001@gmail.com>
2020-03-30 16:54:32 +0500

    Add more pictures and developed text

---
+++ main.py
@@ -0,0 +1,44 @@
+#Import Libraries
+import numpy as np
+
+np.random.seed(1
+        )
+#Class
+class neural_function:
+    """This neural network simulates logic gates"""
+
+    def __init__(self):
+        """This initiates the neural network"""
+
+        pass
+
+    def forward_propagation(self):
+        
+        pass
+
+    def backpropagation(self):
+        
+        pass
+
+    def train():
+        
+        pass
+
+    def RELU():
+        pass
+
+    def RELU2DERIV():
+        pass
+
+
+#Main Function
+def main():
+    
+    pass
+
+#Initiation of Main Function
+if __name__ == '__main__':
+    main()
+
+
+
